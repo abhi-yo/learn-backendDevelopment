ab708b9b843b9e876a64c9f3baa9a524e0486c6f
abhi-yo <akshatsing11@gmail.com>
2023-10-26 08:06:58 +0530

    :sparkles: initial commit

---
+++ FileUpload/index.js
@@ -0,0 +1,28 @@
+// app create
+const express = require("express");
+const app = express();
+
+// PORT find krna hai
+const PORT = process.env.PORT || 3000;
+
+// middleware add krna hai
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
+
+// db se connect krna hai
+const db = require("./config/database");
+db.connect();
+
+// cloud se connect krna hai
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+// api route mount krna hai
+const Upload = require("./routes/FileUpload");
+app.use("api/v1/upload", Upload);
+
+// activate the server
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
